3c7f8e7731ad43f936758d11ed9d8c328b45859d
elinaldo <elinnaldosc@gmail.com>
2022-11-03 00:16:23 -0300

    create routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from '@fastify/cors';
 import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import ShortUniqueId from 'short-unique-id';
+import z from 'zod';
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -16,10 +18,44 @@ async function bootstrap(){
     })
 
     fastify.get('/pools/count', async() => {
-
+        
         const poolsCount = await prisma.pool.count();
         return { poolsCount }
     })
+
+    fastify.get('/users/count', async() => {
+        
+        const usersCount = await prisma.user.count();
+        return { usersCount }
+    })
+
+    fastify.get('/guesses/count', async() => {
+        
+        const guessesCount = await prisma.guess.count();
+        return { guessesCount }
+    })
+    
+    fastify.post('/pools', async(req, res) => {
+
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+        
+        const { title } = createPoolBody.parse(req.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return res.status(201).send({code})
+    })
+   
 
     await fastify.listen({port:3333/* , host: '0.0.0.0' */})
 };
